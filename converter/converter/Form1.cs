e15c3a8149ae04cdcb3151cf05e0bb22b100b9da
TinyPablo <wedio777@gmail.com>
2022-11-08 10:51:24 +0100

    v0.1
    test

--- converter/converter/Form1.cs
+++ converter/converter/Form1.cs
@@ -6,5 +6,10 @@ namespace converter
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
