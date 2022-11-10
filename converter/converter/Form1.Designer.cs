37bf2e4fa657bbe9ec3015f8f90ceb55bc047738
TinyPablo <wedio777@gmail.com>
2022-11-10 21:41:04 +0100

    v1.7 PROJECT ALREADY DONE + FILERS TO BULID

--- converter/converter/Form1.Designer.cs
+++ converter/converter/Form1.Designer.cs
@@ -36,22 +36,24 @@
             this.to_unit_listbox = new System.Windows.Forms.ListBox();
             this.exception_info_label = new System.Windows.Forms.Label();
             this.clear_button = new System.Windows.Forms.Button();
+            this.text_from_label = new System.Windows.Forms.Label();
+            this.text_to_label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // user_input_textbox
             // 
-            this.user_input_textbox.Location = new System.Drawing.Point(93, 31);
+            this.user_input_textbox.Location = new System.Drawing.Point(94, 32);
             this.user_input_textbox.MaxLength = 15;
             this.user_input_textbox.Name = "user_input_textbox";
             this.user_input_textbox.PlaceholderText = "Enter Value";
-            this.user_input_textbox.Size = new System.Drawing.Size(143, 23);
+            this.user_input_textbox.Size = new System.Drawing.Size(131, 23);
             this.user_input_textbox.TabIndex = 0;
             // 
             // calculate_button
             // 
             this.calculate_button.Location = new System.Drawing.Point(104, 119);
             this.calculate_button.Name = "calculate_button";
-            this.calculate_button.Size = new System.Drawing.Size(113, 42);
+            this.calculate_button.Size = new System.Drawing.Size(113, 30);
             this.calculate_button.TabIndex = 1;
             this.calculate_button.Text = "Calculate";
             this.calculate_button.UseVisualStyleBackColor = true;
@@ -59,11 +61,11 @@
             // 
             // result_textbox
             // 
-            this.result_textbox.Location = new System.Drawing.Point(93, 70);
+            this.result_textbox.Location = new System.Drawing.Point(95, 90);
             this.result_textbox.Name = "result_textbox";
             this.result_textbox.PlaceholderText = "Your Result";
             this.result_textbox.ReadOnly = true;
-            this.result_textbox.Size = new System.Drawing.Size(143, 23);
+            this.result_textbox.Size = new System.Drawing.Size(131, 23);
             this.result_textbox.TabIndex = 2;
             // 
             // unit_type_listbox
@@ -73,7 +75,7 @@
             this.unit_type_listbox.Items.AddRange(new object[] {
             "weight",
             "distance"});
-            this.unit_type_listbox.Location = new System.Drawing.Point(7, 31);
+            this.unit_type_listbox.Location = new System.Drawing.Point(18, 27);
             this.unit_type_listbox.Name = "unit_type_listbox";
             this.unit_type_listbox.Size = new System.Drawing.Size(70, 34);
             this.unit_type_listbox.TabIndex = 4;
@@ -83,7 +85,7 @@
             // 
             this.from_unit_listbox.FormattingEnabled = true;
             this.from_unit_listbox.ItemHeight = 15;
-            this.from_unit_listbox.Location = new System.Drawing.Point(7, 85);
+            this.from_unit_listbox.Location = new System.Drawing.Point(25, 88);
             this.from_unit_listbox.Name = "from_unit_listbox";
             this.from_unit_listbox.Size = new System.Drawing.Size(54, 64);
             this.from_unit_listbox.TabIndex = 5;
@@ -92,7 +94,7 @@
             // 
             this.to_unit_listbox.FormattingEnabled = true;
             this.to_unit_listbox.ItemHeight = 15;
-            this.to_unit_listbox.Location = new System.Drawing.Point(269, 85);
+            this.to_unit_listbox.Location = new System.Drawing.Point(241, 88);
             this.to_unit_listbox.Name = "to_unit_listbox";
             this.to_unit_listbox.Size = new System.Drawing.Size(54, 64);
             this.to_unit_listbox.TabIndex = 6;
@@ -101,7 +103,7 @@
             // 
             this.exception_info_label.BackColor = System.Drawing.Color.Transparent;
             this.exception_info_label.ForeColor = System.Drawing.Color.Black;
-            this.exception_info_label.Location = new System.Drawing.Point(117, 9);
+            this.exception_info_label.Location = new System.Drawing.Point(109, 9);
             this.exception_info_label.Name = "exception_info_label";
             this.exception_info_label.Size = new System.Drawing.Size(100, 15);
             this.exception_info_label.TabIndex = 7;
@@ -109,7 +111,7 @@
             // 
             // clear_button
             // 
-            this.clear_button.Location = new System.Drawing.Point(248, 31);
+            this.clear_button.Location = new System.Drawing.Point(229, 32);
             this.clear_button.Name = "clear_button";
             this.clear_button.Size = new System.Drawing.Size(56, 23);
             this.clear_button.TabIndex = 8;
@@ -117,12 +119,32 @@
             this.clear_button.UseVisualStyleBackColor = true;
             this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
             // 
+            // text_from_label
+            // 
+            this.text_from_label.AutoSize = true;
+            this.text_from_label.Location = new System.Drawing.Point(32, 69);
+            this.text_from_label.Name = "text_from_label";
+            this.text_from_label.Size = new System.Drawing.Size(38, 15);
+            this.text_from_label.TabIndex = 9;
+            this.text_from_label.Text = "From:";
+            // 
+            // text_to_label
+            // 
+            this.text_to_label.AutoSize = true;
+            this.text_to_label.Location = new System.Drawing.Point(256, 69);
+            this.text_to_label.Name = "text_to_label";
+            this.text_to_label.Size = new System.Drawing.Size(22, 15);
+            this.text_to_label.TabIndex = 10;
+            this.text_to_label.Text = "To:";
+            // 
             // main_window
             // 
             this.AcceptButton = this.calculate_button;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 184);
+            this.ClientSize = new System.Drawing.Size(319, 161);
+            this.Controls.Add(this.text_to_label);
+            this.Controls.Add(this.text_from_label);
             this.Controls.Add(this.clear_button);
             this.Controls.Add(this.exception_info_label);
             this.Controls.Add(this.to_unit_listbox);
@@ -131,8 +153,6 @@
             this.Controls.Add(this.result_textbox);
             this.Controls.Add(this.calculate_button);
             this.Controls.Add(this.user_input_textbox);
-            this.MaximumSize = new System.Drawing.Size(346, 223);
-            this.MinimumSize = new System.Drawing.Size(346, 223);
             this.Name = "main_window";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Converter";
@@ -152,5 +172,7 @@
         private Label exception_info_label;
         private ListBox unit_type_listbox;
         private Button clear_button;
+        private Label text_from_label;
+        private Label text_to_label;
     }
 }
